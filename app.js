73869ecddc1ce17989a968f75875910fbdb80935
SJWilliams29 <145230889+SJWilliams29@users.noreply.github.com>
2025-11-01 14:18:22 +0000

    Create project files and build function for computer action
    Made the files for the project, built the function to decide
    the computer's action.

---
+++ app.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    let choiceNum = 0;
+    let choice = "";
+    choiceNum = Math.floor(Math.random() * 3);
+    if (choiceNum == 2) {
+        choice = "Rock";
+    } else if (choiceNum == 1) {
+        choice = "Paper";
+    } else {
+        choice = "Scissors";
+    }
+    return choice;
+}

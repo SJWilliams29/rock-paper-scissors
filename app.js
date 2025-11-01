2a7eb7a8c5f050fc35c9942765f5a36e8ae3aad3
SJWilliams29 <145230889+SJWilliams29@users.noreply.github.com>
2025-11-01 16:03:44 +0000

    Add function to play a round
    Added function to make player choice
    case-insensitive, and compare player choice with
    computer choice and determine winner, then
    update scores appropriately.

--- app.js
+++ app.js
@@ -19,3 +19,44 @@ function getHumanChoice() {
     let choice = prompt("Enter your action.");
     return choice;
 }
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice =
+        humanChoice.slice(0, 1).toUpperCase() +
+        humanChoice.slice(1).toLowerCase();
+    switch (humanChoice) {
+        case "Scissors":
+            if (computerChoice == "Rock") {
+                console.log("You lose! Rock beats Scissors.");
+                computerScore++;
+            } else if (computerChoice == "Paper") {
+                console.log("You win! Scissors beats Paper");
+                humanScore++;
+            } else {
+                console.log("Draw!");
+            }
+            break;
+        case "Rock":
+            if (computerChoice == "Paper") {
+                console.log("You lose! Paper beats Rock.");
+                computerScore++;
+            } else if (computerChoice == "Rock") {
+                console.log("You win! Paper beats Rock");
+                humanScore++;
+            } else {
+                console.log("Draw!");
+            }
+            break;
+        case "Paper":
+            if (computerChoice == "Scissors") {
+                console.log("You lose! Scissors beats Paper.");
+                computerScore++;
+            } else if (computerChoice == "Rock") {
+                console.log("You win! Paper beats Rock");
+                humanScore++;
+            } else {
+                console.log("Draw!");
+            }
+            break;
+    }
+}
